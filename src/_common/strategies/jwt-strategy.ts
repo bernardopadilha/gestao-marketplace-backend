17f69bae5c786d87ddf3619b97d833289707c1ad
bernardopadilha <bernardo_limas@hotmail.com>
2025-08-22 18:51:45 -0300

    feat: configurado estrategia de jwt e JwtAuthGuard

---
+++ src/_common/strategies/jwt-strategy.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET')!,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub };
+  }
+}
